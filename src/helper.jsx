77e2f7974c414f2c26255485054b6c82f40eff7a
nickolas-fitkovich <90247187+nickolas-fitkovich@users.noreply.github.com>
2024-03-20 08:06:15 +0100

    .

--- src/helper.jsx
+++ src/helper.jsx
@@ -4,15 +4,6 @@ import FutureIcon from '../src/images/future.png';
 
 export const datesData = [
 	{
-		name: 'ILLIA LISHCHUK(From Sviatlana)',
-		dateOfBirth: 'none',
-		headIcon: NowIcon,
-		login: 'illialishchuk277@gmail.com',
-		password: 'Warszawa2024',
-		range: 'до 30 апреля',
-		notation: 'none',
-	},
-	{
 		name: 'OLEKSII KRYVKO(From Mikalai)',
 		dateOfBirth: 'none',
 		headIcon: NowIcon,
